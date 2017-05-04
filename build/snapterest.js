d18c8807487eab5144c89943b25e5b3e9c440070
Carlsiry <chenkansheng0@163.com>
2017-05-04 14:36:04 +0800

    refactoring with JSX

--- build/snapterest.js
+++ build/snapterest.js
@@ -20502,30 +20502,26 @@ module.exports = require('./lib/React');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// createElement function: React.createElement(type, props, children)
-var h1 = React.createElement('h1', { className: 'header', key: "header" }, 'This is React');
+var listOfItems = React.createElement(
+                    'ul',
+                    { className: 'list-of-items' },
+                    React.createElement(
+                                        'li',
+                                        { className: 'items-1' },
+                                        ' item 1'
+                    ),
+                    React.createElement(
+                                        'li',
+                                        { className: 'items-2' },
+                                        ' item 2'
+                    ),
+                    React.createElement(
+                                        'li',
+                                        { className: 'items-3' },
+                                        ' item 3'
+                    )
+);
 
-var p = React.createElement('p', { className: 'content', key: 'content' }, "And that's how it woiks.");
-
-// Reactfragment []
-var reactFragment = [h1, p];
-
-// factory function: React.createFactory(type)
-var createListItemElement = React.createFactory('li');
-var listItemElement1 = createListItemElement({ className: 'item-1', key: 'item-1' }, 'Item-1');
-var listItemElement2 = createListItemElement({ className: 'item-2', key: 'item-2' }, 'Item-2');
-var listItemElement3 = createListItemElement({ className: 'item-3', key: 'item-3' }, 'Item-3');
-var listItemFragment = [listItemElement1, listItemElement2, listItemElement3];
-
-// inner factory function: React.DOM.ul/li(props, children)
-var todo1 = React.DOM.li({ className: 'todo-1', key: 'todo-1' }, 'todo 1');
-var todo2 = React.DOM.li({ className: 'todo-2', key: 'todo-2' }, 'todo 2');
-var todo3 = React.DOM.li({ className: 'todo-3', key: 'todo-3' }, 'todo 3');
-var todoFragment = [todo1, todo2, todo3];
-var listOfTodos = React.DOM.ul({ className: 'list-of-todos' }, todoFragment);
-
-var section = React.createElement('section', { className: 'container' }, [reactFragment, listItemFragment, listOfTodos]);
-
-ReactDOM.render(section, document.getElementById('react-application'));
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
 
 },{"react":177,"react-dom":26}]},{},[178]);
